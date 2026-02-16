dcc31c9ac507c6832bf9a049a62a79dbd5a125a9
Navaneeth4141 <tnavaneethreddy07@gmail.com>
2026-02-16 10:36:37 +0530

    DAA Programs

---
+++ WEEK - 2/IterativeInOrder.java
@@ -0,0 +1,43 @@
+import java.util.Stack;
+
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int item) {
+        data = item;
+        left = right = null;
+    }
+}
+
+class IterativeInOrder{
+    Node root;
+
+    void inorder() {
+        if (root == null)
+            return;
+
+        Stack<Node> s = new Stack<>();
+        Node curr = root;
+
+        while (curr != null || !s.isEmpty()) {
+            while (curr != null) {
+                s.push(curr);
+                curr = curr.left;
+            }
+            curr = s.pop();
+            System.out.print(curr.data + " ");
+            curr = curr.right;
+        }
+    }
+
+    public static void main(String args[]) {
+        IterativeInOrder tree = new IterativeInOrder();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+        tree.inorder();
+    }
+}

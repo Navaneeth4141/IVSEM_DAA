bcaf7306099a2817f228c69773794f13409727e5
Navaneeth4141 <tnavaneethreddy07@gmail.com>
2026-02-23 21:19:45 +0530

    DAA Programs

---
+++ WEEK - 8/MatrixChainMultiplication.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class MatrixChainMultiplication {
+    static int MatrixChainOrder(int p[], int n) {
+        int[][] dp = new int[n][n];
+        for (int len = 2; len < n; len++) {
+            for (int i = 1; i < n - len + 1; i++) {
+                int j = i + len - 1;
+                dp[i][j] = Integer.MAX_VALUE;
+                for (int k = i; k < j; k++) {
+                    int cost = dp[i][k] + dp[k + 1][j] + p[i - 1] * p[k] * p[j];
+                    if (cost < dp[i][j]) dp[i][j] = cost;
+                }
+            }
+        }
+        return dp[1][n - 1];
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++) arr[i]=sc.nextInt();
+        System.out.println(MatrixChainOrder(arr, n));
+        sc.close();
+    }
+}

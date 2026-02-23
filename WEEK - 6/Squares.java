7da0d9e44ba3c5e57d384fddeee0d0ce1a69c2ca
Navaneeth4141 <tnavaneethreddy07@gmail.com>
2026-02-23 19:26:49 +0530

    DAA Prograns

---
+++ WEEK - 6/Squares.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class Squares{
+    static int minimumCostOfBreaking(Integer X[], Integer Y[], int m, int n){
+        Arrays.sort(X, Collections.reverseOrder());
+        Arrays.sort(Y, Collections.reverseOrder());
+        int hzntl=1, vert=1, i=0, j=0, res=0;
+        while(i<m && j<n){
+            if(X[i]>Y[j]){
+                res+=X[i]*vert;
+                hzntl++;
+                i++;
+            }else{
+                res+=Y[j]*hzntl;
+                vert++;
+                j++;
+            }
+        }
+        while(i<m){
+            res+=X[i]*vert;
+            hzntl++;
+            i++;
+        }
+        while(j<n){
+            res+=Y[j]*hzntl;
+            vert++;
+            j++;
+        }
+        return res;
+    }
+    public static void main(String arg[]){
+        int m=6,n=4;
+        Integer X[]={2,1,3,1,4};
+        Integer Y[]={4,1,2};
+        System.out.print(minimumCostOfBreaking(X,Y,m-1,n-1));
+    }
+}
